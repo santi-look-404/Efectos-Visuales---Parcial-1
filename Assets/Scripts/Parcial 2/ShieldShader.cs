b10a57f0b7962a162f010d443573547a9561aa76
santi-look-404 <santi.l00k.404@gmail.com>
2024-12-03 20:21:30 -0300

    Added Zoom

--- Assets/Scripts/Parcial 2/ShieldShader.cs
+++ Assets/Scripts/Parcial 2/ShieldShader.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class ShieldShader : MonoBehaviour
 {
+    [Header("Zoom")]
+    [SerializeField] string _zoomObjectScreenPositionPropertyName = "_ZoomObjectScreenPosition";
+
+    Camera _camera;
+    Renderer _renderer;
+
+    void Start()
+    {
+        _camera = Camera.main;
+
+        _renderer = GetComponent<Renderer>();
+    }
+
     void Update()
     {
         LookAtCamera();
@@ -9,6 +22,15 @@ public class ShieldShader : MonoBehaviour
 
     private void LookAtCamera()
     {
-        transform.forward = Camera.main.transform.position - transform.position;
+        transform.forward = _camera.transform.position - transform.position;
+    }
+
+    private void Zoom()
+    {
+        Vector3 screenPoint = _camera.WorldToScreenPoint(transform.position);
+
+        Vector3 zoom = new Vector3(screenPoint.x / Screen.width, screenPoint.y / Screen.height, screenPoint.z / Screen.width);
+
+        _renderer.material.SetVector(_zoomObjectScreenPositionPropertyName, zoom);
     }
 }
