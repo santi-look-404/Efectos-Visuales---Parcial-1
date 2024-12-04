09b516bd79788c4f43813265b50ce2a0794316ee
santi-look-404 <santi.l00k.404@gmail.com>
2024-12-04 13:43:32 -0300

    Shield can be invoked

--- Assets/Scripts/Parcial 2/ShieldShader.cs
+++ Assets/Scripts/Parcial 2/ShieldShader.cs
@@ -1,11 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class ShieldShader : MonoBehaviour
 {
-    [Header("Zoom")]
+    [Header("Key")]
+    [SerializeField] string _disolvePropertyName = "_DissolveValue";
     [SerializeField] string _zoomObjectScreenPositionPropertyName = "_ZoomObjectScreenPosition";
 
+    [Header("Disolve")]
+    [SerializeField] float _disolveSpeed;
+
+    bool _shieldOn;
     Camera _camera;
+    Coroutine _disolveCoroutine;
     Renderer _renderer;
 
     void Start()
@@ -20,6 +27,8 @@ public class ShieldShader : MonoBehaviour
         LookAtCamera();
 
         Zoom();
+
+        ToggleShield();
     }
 
     private void LookAtCamera()
@@ -34,5 +43,48 @@ public class ShieldShader : MonoBehaviour
         Vector3 zoom = new Vector3(screenPoint.x / Screen.width, screenPoint.y / Screen.height, screenPoint.z / Screen.width);
 
         _renderer.material.SetVector(_zoomObjectScreenPositionPropertyName, zoom);
+    }
+
+    private void ToggleShield()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ActivateShield();
+        }
+    }
+
+    private void ActivateShield()
+    {
+        float target = 1;
+
+        if (_shieldOn)
+        {
+            target = 0;
+        }
+
+        _shieldOn = !_shieldOn;
+
+        if (_disolveCoroutine != null)
+        {
+            StopCoroutine(_disolveCoroutine);
+        }
+
+        _disolveCoroutine = StartCoroutine(Coroutine_DisolveShield(target));
+    }
+
+    private IEnumerator Coroutine_DisolveShield(float target)
+    {
+        float start = _renderer.material.GetFloat(_disolvePropertyName);
+
+        float lerp = 0;
+
+        while (lerp < 1)
+        {
+            _renderer.material.SetFloat(_disolvePropertyName, Mathf.Lerp(start, target, lerp));
+
+            lerp += Time.deltaTime * _disolveSpeed;
+
+            yield return null;
+        }
     }
 }
